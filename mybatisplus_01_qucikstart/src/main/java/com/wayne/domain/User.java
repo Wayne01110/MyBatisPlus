ba02bd8dccfb5acfc5863dc91ae26071c87035b0
“Wayne” <237512610@qq.com>
2022-12-07 10:06:37 +0800

    commit

--- mybatisplus_01_qucikstart/src/main/java/com/wayne/domain/User.java
+++ mybatisplus_01_qucikstart/src/main/java/com/wayne/domain/User.java
@@ -1,7 +1,10 @@
 package com.wayne.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.Setter;
 
+@Data
 public class User {
     private Long id;
     private String name;
@@ -9,54 +12,4 @@ public class User {
     private Integer age;
     private String tel;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", tel='" + tel + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
 }
--- mybatisplus_01_qucikstart/src/test/java/com/wayne/Mybatisplus01QucikstartApplicationTests.java
+++ mybatisplus_01_qucikstart/src/test/java/com/wayne/Mybatisplus01QucikstartApplicationTests.java
@@ -1,6 +1,8 @@
 package com.wayne;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wayne.dao.UserDao;
 import com.wayne.domain.User;
 import org.junit.jupiter.api.Test;
@@ -17,9 +19,50 @@ class Mybatisplus01QucikstartApplicationTests {
     private UserDao userDao;
 
     @Test
+    void testSave() {
+        User user = new User();
+        user.setName("刘自成");
+        user.setPassword("it");
+        user.setAge(25);
+        user.setTel("400655655");
+        userDao.insert(user);
+    }
+
+    @Test
+    void testDelete() {
+        userDao.deleteById(1600089137870630914L);
+    }
+
+    @Test
+    void testUpdate() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("tom666");
+        user.setPassword("tom888");
+        userDao.updateById(user);
+    }
+
+    @Test
+    void testGetById() {
+        User user = userDao.selectById(2L);
+        System.out.println(user);
+    }
+
+    @Test
     void testGetAll() {
         List<User> userList = userDao.selectList(null);
         System.out.println(userList);
+    }
+
+    @Test
+    void testGetByPage() {
+        IPage page = new Page(1, 2);
+        userDao.selectPage(page, null);
+        System.out.println("当前页码值：" + page.getCurrent());
+        System.out.println("每页显示数：" + page.getSize());
+        System.out.println("一共多少页：" + page.getPages());
+        System.out.println("一共多少条数据：" + page.getTotal());
+        System.out.println("数据：" + page.getRecords());
     }
 
 }
